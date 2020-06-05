600538d755505886209a50026490b671e71a2de0
Gustavo Melo <gustavo.ghcmelo@gmail.com>
2020-06-04 21:36:39 -0300

    criada a classe do template

---
+++ vendor/verdanatech/php-classes/src/Page.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Verdanatech;
+
+use Rain\Tpl;
+
+class Page {
+
+    private $tpl;
+    private $options = [];
+    private $defaults = [
+        "data"=>[]
+    ];
+
+    public function __construct($opcoes = array()) {
+        
+        $this->options = array_merge($this->defaults, $opcoes);
+
+        $config = array(
+            "tpl_dir"       => $_SERVER["DOCUMENT_ROOT"] . "/viewes/",
+            "cache_dir"     => $_SERVER["DOCUMENT_ROOT"] . "/viewes-cache/",
+            "debug"         => false, // set to false to improve the speed
+        );
+
+        Tpl::configure( $config );
+
+        $this->tpl = new Tpl;
+
+        foreach($this->options["data"] as $key => $values) {
+            $this->tpl->assign($key, $value);
+        }
+
+        $this->tpl->draw("header");
+    }
+
+    public function setTpl($name, $data = array(), $returnHTML = false) {
+
+        foreach($this->options["data"] as $key => $values) {
+            $this->tpl->assign($key, $value);
+        }
+
+        return $this->tpl->draw($name, $returnHTML);
+
+    }
+
+    public function __destruct() {
+
+        $this->tpl->draw("footer");
+    }
+}
+?>
